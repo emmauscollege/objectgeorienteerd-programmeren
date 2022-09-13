316fe2d4b2bf3a760a65cfb6234412d6584c7e3c
A.P. Cammeraat <cam@emmauscollege.nl>
2022-09-13 11:35:52 +0000

    met dokter

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 /* Klassendefinities                             */
 /* ********************************************* */
 
-class Actor {
+class Mens {
   x;
   y;
   speedX;
@@ -21,31 +21,28 @@ class Actor {
 
   isBesmet;
 
-  /**
-   * constructor van abstracte klasse
-   * initialiseert de attributen x, y, speedX, speedY
-   * 
-   * LET OP: subklassen MOETEN zelf this.breedte
-   * in de constructor definiëren.
-   */
-  constructor(x, y, speedX, speedY) {
-    this.x = x;
-    this.y = y;
-    this.speedX = speedX;
-    this.speedY = speedY;
+  constructor(newX, newY, newSpeedX, newSpeedY) {
+    this.x = newX;
+    this.y = newY;
+    this.speedX = newSpeedX;
+    this.speedY = newSpeedY;
+    this.breedte = 20;
 
     this.isBesmet = false;
   }
-  /**
-   * show is een abstracte methode
-   * moet worden overschreven door subklassen
-   */
-  show() {}
 
-  /**
-   * update de positie en 'spiegelt' de snelheden
-   * wanneer het object tegen de randen botst
-   */
+  show() {
+    noStroke();
+    if (this.isBesmet === true) {
+      fill(255, 0, 0);      // rood
+    }
+    else {
+      fill(255, 255, 255);  // wit
+    }
+
+    rect(this.x, this.y, this.breedte, this.breedte);
+  }
+
   update() {
     // update positie
     this.x = this.x - this.speedX;
@@ -61,35 +58,31 @@ class Actor {
     }
   }
 
-  /**
-   * berekent of 'this' Actor-object overlapt
-   * met een ander actorobject genaamd andereActor
-   */
-  isOverlappend(andereActor) {
+  isOverlappend(andereMens) {
     // zet teruggeefwaarde standaard op false
     var overlappend = false;
   
-    if ( // valt linkerbovenhoek binnen randen van 'andereActor'?
-         (this.x >= andereActor.x &&
-          this.x <= andereActor.x + andereActor.breedte &&
-          this.y >= andereActor.y &&
-          this.y <= andereActor.y + andereActor.breedte)
+    if ( // valt linkerbovenhoek binnen randen van 'andereMens'?
+         (this.x >= andereMens.x &&
+          this.x <= andereMens.x + andereMens.breedte &&
+          this.y >= andereMens.y &&
+          this.y <= andereMens.y + andereMens.breedte)
         ||
-         // OF valt rechterbovenhoek binnen randen van 'andereActor'?
-         (this.x + this.breedte >= andereActor.x &&
-          this.x + this.breedte <= andereActor.x + andereActor.breedte &&
-          this.y >= andereActor.y &&
-          this.y <= andereActor.y + andereActor.breedte)
+         // OF valt rechterbovenhoek binnen randen van 'andereMens'?
+         (this.x + this.breedte >= andereMens.x &&
+          this.x + this.breedte <= andereMens.x + andereMens.breedte &&
+          this.y >= andereMens.y &&
+          this.y <= andereMens.y + andereMens.breedte)
         || // OF de linkeronderhoek?
-         (this.x >= andereActor.x &&
-          this.x <= andereActor.x + andereActor.breedte &&
-          this.y + this.breedte >= andereActor.y &&
-          this.y + this.breedte <= andereActor.y + andereActor.breedte)
+         (this.x >= andereMens.x &&
+          this.x <= andereMens.x + andereMens.breedte &&
+          this.y + this.breedte >= andereMens.y &&
+          this.y + this.breedte <= andereMens.y + andereMens.breedte)
         || // OF de hoek rechtsonder?
-         (this.x >= andereActor.x &&
-          this.x <= andereActor.x + andereActor.breedte &&
-          this.y + this.breedte >= andereActor.y &&
-          this.y + this.breedte <= andereActor.y + andereActor.breedte)
+         (this.x >= andereMens.x &&
+          this.x <= andereMens.x + andereMens.breedte &&
+          this.y + this.breedte >= andereMens.y &&
+          this.y + this.breedte <= andereMens.y + andereMens.breedte)
        ) {
           
       overlappend = true;
@@ -100,35 +93,23 @@ class Actor {
   }
 }
 
-class Mens extends Actor {
-  constructor(x, y, speedX, speedY) {
-    // roep de constructor van Actor aan
-    super(x, y, speedX, speedY);
+class Kat {
+  x;
+  y;
+  speedX;
+  speedY;
+  breedte;
 
-    // geef breedte een correcte waarde
-    this.breedte = 20;
-  }
+  isBesmet;
 
-  show() {
-    noStroke();
-    if (this.isBesmet === true) {
-      fill(255, 0, 0);      // rood
-    }
-    else {
-      fill(255, 255, 255);  // wit
-    }
-
-    rect(this.x, this.y, this.breedte, this.breedte);
-  }
-}
-
-class Kat extends Actor {
-  constructor(x, y, speedX, speedY) {
-    // roep de constructor van Actor aan
-    super(x, y, speedX, speedY);
-
-    // geef breedte een correcte waarde
+  constructor(newX, newY, newSpeedX, newSpeedY) {
+    this.x = newX;
+    this.y = newY;
+    this.speedX = newSpeedX;
+    this.speedY = newSpeedY;
     this.breedte = 10;
+
+    this.isBesmet = false;
   }
 
   show() {
@@ -141,6 +122,68 @@ class Kat extends Actor {
     }
 
     rect(this.x, this.y, this.breedte, this.breedte);
+  }
+
+  update() {
+    // update positie
+    this.x = this.x - this.speedX;
+    this.y = this.y - this.speedY;
+
+    // stuiter tegen randen
+    if (this.x <= 0 || this.x + this.breedte >= width) {
+      this.speedX = this.speedX * -1;
+    }
+
+    if (this.y <= 0 || this.y + this.breedte >= height) {
+      this.speedY = this.speedY * -1;
+    }
+  }
+
+  isOverlappend(andereKat) {
+    // zet teruggeefwaarde standaard op false
+    var overlappend = false;
+  
+    if ( // valt linkerbovenhoek binnen randen van 'andereKat'?
+         (this.x >= andereKat.x &&
+          this.x <= andereKat.x + andereKat.breedte &&
+          this.y >= andereKat.y &&
+          this.y <= andereKat.y + andereKat.breedte)
+        ||
+         // OF valt rechterbovenhoek binnen randen van 'andereKat'?
+         (this.x + this.breedte >= andereKat.x &&
+          this.x + this.breedte <= andereKat.x + andereKat.breedte &&
+          this.y >= andereKat.y &&
+          this.y <= andereKat.y + andereKat.breedte)
+        || // OF de linkeronderhoek?
+         (this.x >= andereKat.x &&
+          this.x <= andereKat.x + andereKat.breedte &&
+          this.y + this.breedte >= andereKat.y &&
+          this.y + this.breedte <= andereKat.y + andereKat.breedte)
+        || // OF de hoek rechtsonder?
+         (this.x >= andereKat.x &&
+          this.x <= andereKat.x + andereKat.breedte &&
+          this.y + this.breedte >= andereKat.y &&
+          this.y + this.breedte <= andereKat.y + andereKat.breedte)
+       ) {
+        
+      overlappend = true;
+    }
+  
+    // stuur de teruggeefwaarde terug
+    return overlappend;
+  }
+}
+
+class Dokter extends Mens {
+  show() {
+    // teken zoals de klasse Mens dat doet
+    super.show();
+
+    // en daarna nog een rood kruis
+    strokeWeight(5);
+    stroke(255, 0, 0);    // rood
+    line(this.x + this.breedte / 2, this.y, this.x + this.breedte / 2, this.y + this.breedte);
+    line(this.x, this.y + this.breedte / 2, this.x + this.breedte, this.y + this.breedte / 2);
   }
 }
 
@@ -203,6 +246,9 @@ function setup() {
     actoren.push(nieuweKat);
   }
 
+  // maak 1 dokter (random waarden zijn nog mooier)
+  actoren.push(new Dokter(width / 2, height / 2, 3, 5));
+
   actoren[0].isBesmet = true;
 }
 
@@ -242,10 +288,19 @@ function draw() {
         if (actorenOverlappen) {
           // check of er een besmetting optreedt
           if (actorA.isBesmet || actorB.isBesmet) {
-            // als er één besmet is, wordt ze allebei besmet
-            // als ze allebei besmet zijn, verandert deze code niets.
-            actorA.isBesmet = true;
-            actorB.isBesmet = true;
+            if (actorA instanceof Dokter || actorB instanceof Dokter) {
+              // minimaal één van de mensen is dokter,
+              // dus ze worden / blijven beide gezond
+              actorA.isBesmet = false;
+              actorB.isBesmet = false;
+            }
+            else {
+              // geen van de mensen is dokter, dus
+              // als er één besmet is, wordt ze allebei besmet
+              // als ze allebei besmet zijn, verandert deze code niets.
+              actorA.isBesmet = true;
+              actorB.isBesmet = true;
+            }
           }
         }
       }
